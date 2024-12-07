dbc0e44766dd59a072b1a160e07133404cc04f8d
unknown <wyatt.probst@okstate.edu>
2024-12-06 22:23:03 -0600

    data.yaml fix

--- validate.py
+++ validate.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 model_path = "weights.pt"
-data_path = "C:/Users/steve/Documents/GitHub/4273Project2/datasets/data.yaml"
+data_path = "data.yaml"
 
 model = YOLO(model_path)
 results = model.val(data=data_path)
